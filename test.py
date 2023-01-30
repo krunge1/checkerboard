f9c8a819b2a13f456431eaca8e75a26fd0103bf3
Kyle Runge <krunge1@hotmail.com>
2023-01-29 20:44:22 -0600

    test file

---
+++ test.py
@@ -0,0 +1,33 @@
+for i in range(1, 8):
+    if i%2==0
+        print (5)
+    else
+        print (2)
+
+
+
+        # 
+        # 
+        # 
+        # 
+        # {%for i in range(1,x+1):%}
+        #     <div class="row">
+        #         {%if i % 2 == 0%}
+        #             {%for i in range(1,y+1):%}
+        #                 {%if i % 2 == 0%}
+        #                 <div class="block">primary</div>
+        #                 {%endif%}
+        #             {%else%}
+        #                 <div class="block">secondary</div>
+        #             {%endfor%}
+        #         {%else%}
+        #             {%for i in range(1,y+1):%}
+        #                 {%if i % 2 == 0%}
+        #                 <div class="block">secondary2</div>
+        #                 {%endif%}
+        #             {%else%}
+        #                 <div class="block">primary2</div>
+        #             {%endfor%}
+        #         {%endif%}
+        #     </div>
+        # {%endfor%}
